7b04c46856398d44ef1d4f6f5b86093a6350f359
BeckyBoo88 <rbooco07@gmail.com>
2021-07-20 15:59:46 -0600

    problem one solved

--- 1-character-count.py
+++ 1-character-count.py
@@ -29,4 +29,17 @@ the character d occurs 1 times
 '''
 
 def character_count(string):
-  pass
+  hash_table = { }
+  for letter in string:
+    #print(letter)
+    hash_table[letter] = string.count(letter)
+  for letter, count in hash_table.items():
+    #print(letter, count)
+    print(f'the character {letter} occurs {count} times')
+    #print(hash_table)
+
+  #return hash_table
+
+character_count('hello world')
+# print(character_count('hello world'))
+# print(character_count(f'the characher {letter} occurs {letter.number} times'))
